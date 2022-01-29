14c1b0bf8cf735f3d7ace03209deced536815267
Alvaro Augusto Pereira <alvaro.pereira@ibm.com>
2022-01-29 16:41:27 -0300

    component atuação profissional

--- src/app/atuacao-profissional/atuacao-profissional.component.ts
+++ src/app/atuacao-profissional/atuacao-profissional.component.ts
@@ -7,6 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AtuacaoProfissionalComponent implements OnInit {
 
+  atividadesIBM = [
+    '- Participação em treinamentos em diversas áreas de TI, com ênfase em DevSecOps.',
+    '- Implantar aplicativos via processo automatizado.',
+    '- Ansible/Ansible Tower.',
+    '- Linux',
+    '- Scrum',
+    '- SQL Server',
+    '- Mongo DB',
+    '- Angular',
+    '- Azure Monitoring'
+        ]
+  
+  atividadesMktNow = [
+    '- Reunir informações relevantes (dados dos clientes através de um sistema Google Analyticstc, por exemplo).',
+    '- Limpeza de dados (identificação, correção de informações duplicadas, inválidas ou corrompidas).',
+    '- Criar e desenvolver relatórios gerenciais.',
+    '- Automatizar documentos para uso recorrente.',
+    '- Montagem de relatórios, análise, apresentação de resultados.'
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/atuacao-profissional/atuacao-profissional.module.ts
+++ src/app/atuacao-profissional/atuacao-profissional.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatListModule} from '@angular/material/list';
+import { AtuacaoProfissionalComponent } from './atuacao-profissional.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AtuacaoProfissionalComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    MatIconModule,
+    NgbModule,
+    MatListModule
+    
+    
   ]
 })
 export class AtuacaoProfissionalModule { }
