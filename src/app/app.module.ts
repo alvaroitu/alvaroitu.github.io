2226cfb686c0206c84674a20c05aeedfcb7d7264
Álvaro Itu <contato@alvaroitu.com>
2022-07-29 06:41:18 -0300

    fix deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
@@ -39,7 +40,7 @@ import { AppComponent } from './app.component';
     ConhecaItuModule,
     ContatoModule
   ],
-  providers: [],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
